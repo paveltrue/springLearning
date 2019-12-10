bbca9c7dc7cea2902f5d777a25d214b8b450dd1e
Pavel <yanpavqa@gmail.com>
2019-12-10 22:30:34 +0300

    small additionals

---
+++ src/main/java/letsCode/repos/MessageRepo.java
@@ -0,0 +1,7 @@
+package letsCode.repos;
+
+import letsCode.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MessageRepo extends CrudRepository<Message, Long> {
+}
