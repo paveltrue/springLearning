88a579b587c7e617158063ce9eb69d75f1ad796c
yanushkevich_p <PA1212ch>
2019-12-16 17:39:04 +0300

    security added

--- src/main/java/letsCode/repos/MessageRepo.java
+++ src/main/java/letsCode/repos/MessageRepo.java
@@ -3,5 +3,9 @@ package letsCode.repos;
 import letsCode.domain.Message;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface MessageRepo extends CrudRepository<Message, Long> {
+
+    List<Message> findByTag(String tag);
 }
